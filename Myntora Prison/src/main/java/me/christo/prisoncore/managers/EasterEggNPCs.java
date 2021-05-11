53ad4f84ac51e15c7643c35ffe758f106c014edd
zaymyntora <zay@myntora.net>
2021-05-10 23:47:41 -0400

    farming npc

--- Myntora Prison/src/main/java/me/christo/prisoncore/managers/EasterEggNPCs.java
+++ Myntora Prison/src/main/java/me/christo/prisoncore/managers/EasterEggNPCs.java
@@ -19,6 +19,8 @@ public class EasterEggNPCs implements Listener {
         ServersNPC.createNPC(1001, NPCType.PLAYER, new Location(Bukkit.getWorld("prison_spawn"), -7, 100, 5, -130, 0), "Bob");
         ServersNPC.createNPC(1002, NPCType.PLAYER, new Location(Bukkit.getWorld("prison_spawn"), 6, 98, -90, 115, 0), "Christo");
         ServersNPC.createNPC(1003, NPCType.PLAYER, new Location(Bukkit.getWorld("prison_spawn"), 33, 98, -49.5, 89, 0), "Nurse");
+        ServersNPC.createNPC(1004, NPCType.PLAYER, new Location(Bukkit.getWorld("prison_spawn"), 129, 92, 29, 145, 0), "Farmer");
+        ServersNPC.createNPC(1005, NPCType.PLAYER, new Location(Bukkit.getWorld("prison_spawn"), -20, 98, -78, -32, 0), "Cactus");
     }
 
     @EventHandler
@@ -46,6 +48,16 @@ public class EasterEggNPCs implements Listener {
 
         if(event.getNpc().equals(nurse)) {
             Infirmary.openGui(player);
+        }
+        ZNPC farmer = ZNPC.find(1004);
+
+        if(event.getNpc().equals(farmer)) {
+            player.sendMessage(Color.prison("NPC", "I've got all your farming needs"));
+        }
+        ZNPC cactus = ZNPC.find(1005);
+
+        if(event.getNpc().equals(cactus)) {
+            player.sendMessage(Color.prison("NPC", "Can you believe Zay coded this?!"));
         }
     }
 
